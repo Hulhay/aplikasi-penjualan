4cbf16ac37d0b8de413c75745507c44a7853d572
Hulhay <prasetyosyifa@gmail.com>
2022-03-19 00:50:22 +0700

    Menambahkan page registrasi, login, 404 ke src/pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
+import Registrasi from './pages/registrasi';
+import Login from './pages/login';
+import NotFound from './pages/404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/registrasi" element={<Registrasi />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/404/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotFound() {
+	
+	return <h1>404: Halaman NotFound</h1>	
+
+}
+
+export default NotFound;
