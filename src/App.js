4f493dd1adefe34ad4e35add56f45e52dbe3ebe9
Hulhay <prasetyosyifa@gmail.com>
2022-03-19 22:51:27 +0700

    Menambah private route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Outlet} from 'react-router-dom';
 
 import Registrasi from './pages/registrasi';
 import Login from './pages/login';
 import NotFound from './pages/404';
-import Pengaturan from './pages/private/pengaturan';
 import Pengguna from './pages/private/pengaturan/pengguna';
 import Toko from './pages/private/pengaturan/toko';
-import Produk from './pages/private/produk';
 import GridProduk from './pages/private/produk/grid';
 import EditProduk from './pages/private/produk/edit';
 import Home from './pages/private/home';
 import Transaksi from './pages/private/transaksi';
-
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         
         <Route path="registrasi" element={<Registrasi />} />
         <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="transaksi" element={<Transaksi />} />
+        <Route path="/" element={<PrivateRoute><Home /></ PrivateRoute>} />
+        <Route path="transaksi" element={<PrivateRoute><Transaksi /></PrivateRoute>} />
         
-        <Route path="pengaturan" element={<Pengaturan />} >
+        <Route path="pengaturan" element={<PrivateRoute><Outlet /></PrivateRoute>} >
           <Route path="pengguna" element={<Pengguna />} />
           <Route path="toko" element={<Toko />} />
         </Route>
 
-        <Route path="produk" element={<Produk />} >
+        <Route exact path="produk" element={<PrivateRoute><Outlet /></PrivateRoute>} >
           <Route path="grid" element={<GridProduk />} />
           <Route path="edit/:produkId" element={<EditProduk />} />
         </Route>
       
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
